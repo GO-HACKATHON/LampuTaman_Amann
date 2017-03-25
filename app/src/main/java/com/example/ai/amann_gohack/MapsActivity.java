48d4fb027bdbbee526c1c9780c4197cf6a08dcb4
irfanhanif <if.irfanhanif@gmail.com>
2017-03-26 00:53:30 +0700

    Penambahan fitur menandai daerah rawan

--- app/src/main/java/com/example/ai/amann_gohack/MapsActivity.java
+++ app/src/main/java/com/example/ai/amann_gohack/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.ai.amann_gohack;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -13,18 +14,29 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,7 +47,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,6 +60,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager myLocationManager;
     private TheLocationListener myLocationListener;
     private Marker currentMarker;
+    private Button btnTandai, btnEmergency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +69,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnTandai = (Button) findViewById(R.id.btnTandai);
+        btnTandai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                Location location = null;
+                while (location == null){
+                    location = myLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) break;
+                    else location = myLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+                final LayoutInflater inflater = MapsActivity.this.getLayoutInflater();
+
+                final View dialogContent = inflater.inflate(R.layout.mark_area, null);
+
+                builder.setView(dialogContent);
+
+                final TextView etLatitude = (TextView) dialogContent.findViewById(R.id.etLatitude);
+                final TextView etLongitude = (TextView) dialogContent.findViewById(R.id.etLongitude);
+                final EditText etDeskripsi = (EditText) dialogContent.findViewById(R.id.etDeskripsi);
+
+                final String lat = Double.toString(location.getLatitude());
+                final String lng = Double.toString(location.getLongitude());
+
+                etLatitude.setText(lat);
+                etLongitude.setText(lng);
+
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                final AlertDialog dialog = builder.create();
+
+                final Button positiveButton = (Button) dialogContent.findViewById(R.id.posButton);
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String deskripsi = etDeskripsi.getText().toString();
+                        MapsActivity.this.insertMarker(Double.parseDouble(lat), Double.parseDouble(lng), deskripsi);
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+        btnEmergency = (Button)findViewById(R.id.btnEmergency);
+        btnEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // DIISI OLEH IKHSAN
+            }
+        });
     }
 
     @Override
@@ -189,5 +272,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
         builder.create().show();
+    }
+
+    private void insertMarker(final Double latitude, final Double longitude, final String deskripsi){
+        Log.d("url", Config.api_domain+"insertpin");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config.api_domain+"insertpin",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.d("RESPONSE", "resp" + response);
+                            JSONObject get = new JSONObject(response);
+                            String resp = get.getString("response");
+                            String message = get.getString("message");
+
+                            if(resp.equals("500")){
+                                Toast.makeText(MapsActivity.this, message,
+                                        Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            else if(resp.equals("200")){
+                                Toast.makeText(MapsActivity.this, message,
+                                        Toast.LENGTH_LONG).show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley Error", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                SharedPreferences prefs = getSharedPreferences(Config.MY_PREFS_NAME, MODE_PRIVATE);
+                String email = prefs.getString("email", "null");
+                Log.d("email", email);
+
+                //String nama_daerah = getAreaName(lat, lng);
+                String nama_daerah = "Indonesia";
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("key", Config.key);
+                params.put("email_pengguna", email);
+                params.put("nama_daerah", nama_daerah);
+                params.put("deskripsi_daerah", deskripsi);
+                params.put("lat_daerah", Double.toString(latitude));
+                params.put("lng_daerah", Double.toString(longitude));
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(MapsActivity.this);
+        requestQueue.add(stringRequest);
+    }
+
+    private String getAreaName(Double latitude, Double longitude){
+        String result = null;
+        return result;
     }
 }
